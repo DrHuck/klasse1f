db32004654254663ab33d8a4bea0b757b00b7a1c
DrHuck <antal.huck@edubs.ch>
2023-06-14 09:06:13 +0200

    created fibonacci sequence

---
+++ demo.py
@@ -0,0 +1,15 @@
+def calculate_next_fibonacci_number(a1, a2):
+    return a1 + a2
+
+
+a_old = 1
+a_current = 1
+
+print(a_old)
+print(a_current)
+
+for k in range(20):
+    swap = a_current
+    a_current = calculate_next_fibonacci_number(a_current, a_old)
+    print(a_current)
+    a_old = swap
